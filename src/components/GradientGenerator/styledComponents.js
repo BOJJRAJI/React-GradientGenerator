835ce4bc24e5dd184791a03c1d825ed5af00dfb3
BOJJA RAJI <94085980+BOJJRAJI@users.noreply.github.com>
2023-10-20 10:47:00 +0530

    Update styledComponents.js

--- src/components/GradientGenerator/styledComponents.js
+++ src/components/GradientGenerator/styledComponents.js
@@ -1,100 +1,77 @@
 import styled from 'styled-components'
 
-export const AppContainer = styled.div`
+export const BgContainer = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
   align-items: center;
-  background-image: linear-gradient(${props => props.gradientValue});
+  justify-content: center;
+  width: 100%;
   min-height: 100vh;
-`
-
-export const ResponsiveContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  width: 90%;
-  max-width: 550px;
-  @media screen and (min-width: 768px) {
-    width: 85%;
-    max-width: 1110px;
-  }
+  background-color: #014f7b;
+  background-image: linear-gradient(${props => props.gradientValue});
 `
 
 export const Heading = styled.h1`
-  color: #ffffff;
-  font-family: 'Roboto';
-  font-size:  30px
+  font-size: 25px;
+  color: white;
   font-weight: bold;
+  font-family: 'Roboto';
 `
 
-export const InputsContainer = styled.div`
+export const Text = styled.p`
+  font-size: 15px;
+  color: #ffffff79;
+  font-weight: bold;
+  font-family: 'Roboto';
+`
+
+export const ButtonsContainer = styled.ul`
   display: flex;
   flex-direction: row;
-  width: 20%;
   justify-content: space-between;
-  margin-bottom: 20px;
+  width: 30%;
 `
 
-export const ColorInputContainer = styled.div`
-  display: flex;
-  flex-direction: column;
+export const List = styled.li`
+  list-style-type: none;
 `
 
-export const LabelEle = styled.label`
-  color: #ffffff;
-  font-family: 'Roboto';
-  font-size: 18px;
-  font-weight: 500;
-`
-
-export const InputElement = styled.input`
-  border: 0px;
-  color: ${props => props.bgColor};
-  background-color: transparent;
-  height: 40px;
-  width: 100px;
-`
-
-export const CustomButton = styled.button`
+export const Button = styled.button`
   border: none;
+  padding: 10px;
+  border-radius: 5px;
+  color: #1e293b;
+
+  font-size: 15px;
+  width: 70px;
+  font-weight: 700;
+  font-family: 'Roboto';
+  background-color: #ffffff;
+  opacity: ${props => (props.isActive ? 1 : 0.5)};
+`
+
+export const InputColorsContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  width: 20%;
+`
+
+export const Input = styled.input`
+  padding: 10px;
+  width: 100%;
+  background-color: ${props => props.bgColor};
+  border: none;
+`
+export const Generate = styled.button`
+  border: none;
+  padding: 10px;
+  border-radius: 6px;
   background-color: #00c9b7;
   color: #1e293b;
-  padding: 10px;
-  border-radius: 7px;
-`
-
-export const CustomUnOrderedList = styled.ul`
-  padding-left: 0px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  width: 50%;
-`
-export const paraElement = styled.p`
-  color: #ffffff;
-  font-family: 'Roboto';
-  font-size: 20px;
-  font-weight: 700;
-`
-export const DirectionsDescription = styled.p`
-  color: #ededed;
-  font-family: 'Roboto';
-  font-weight: 500;
-  font-size: 22px;
-  line-height: 1.5;
-`
-
-export const ColorValue = styled.p`
-  color: #ffffff;
-  font-size: 18px;
-  font-weight: 500;
-`
-export const ColorsPickersDescription = styled.p`
-  color: #ededed;
-  font-family: 'Roboto';
-  font-weight: 500;
-  font-size: 22px;
-  line-height: 1.5;
+  height: 50px;
+  margin-top: 20px;
+  font-size: 15px;
+  font-weight: 600;
 `
